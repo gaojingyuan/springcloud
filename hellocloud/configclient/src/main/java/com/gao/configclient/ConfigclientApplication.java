3f7e8ffeb0aaefecae08728142d0a2cf1bb02593
gaojingyuan <gaojingyuanyuan@126.com>
2018-01-10 17:23:36 +0800

    modify config

--- hellocloud/configclient/src/main/java/com/gao/configclient/ConfigclientApplication.java
+++ hellocloud/configclient/src/main/java/com/gao/configclient/ConfigclientApplication.java
@@ -1,12 +1,26 @@
 package com.gao.configclient;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class ConfigclientApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ConfigclientApplication.class, args);
 	}
+
+	@Value("${project}")
+	String project;
+
+	@RequestMapping(value = "/hi")
+	public String hi(){
+		return project;
+	}
+
+
 }
