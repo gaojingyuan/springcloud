abe4b697dc1a4e7aa290892228caba7544b2434b
gaojingyuan <gaojingyuanyuan@126.com>
2018-01-29 16:50:19 +0800

    add turbine

--- hellocloud/eurekaclient/src/main/java/com/gao/eurekaclient/EurekaclientApplication.java
+++ hellocloud/eurekaclient/src/main/java/com/gao/eurekaclient/EurekaclientApplication.java
@@ -1,9 +1,12 @@
 package com.gao.eurekaclient;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,14 +15,21 @@ import org.springframework.web.servlet.ModelAndView;
 @SpringBootApplication
 @EnableEurekaClient
 @RestController
+@EnableHystrix
+@EnableHystrixDashboard
 public class EurekaclientApplication {
 
     @Value("${server.port}")
     private String port;
 
     @RequestMapping("/hi")
+    @HystrixCommand(fallbackMethod = "hiError")
     public String home(@RequestParam String name) {
         return "hi "+name+",i am from port:" +port;
+    }
+
+    public String hiError(String name) {
+        return "hi,"+name+",sorry,error!";
     }
 
     @RequestMapping("/test")
